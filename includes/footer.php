80b1ba36aa6ec448cdfcd98426b563f79503b4c5
Nick <zuzownic@msu.edu>
2014-06-10 22:42:59 -0400

    Changed the file endings from html to php

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="css/electric.css" type="text/css" media="screen" title="master" charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen" title="master" charset="utf-8">
+</head>
+
+<footer id="footer" class="well">
+    <div class="div1">
+        <p>This is the first side</p>
+    </div>
+    <div class="div2">
+        <p>This is the second side</p>
+    </div>
+</footer>
+</html>
